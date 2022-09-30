8f19369bb64e67d719ab578a76d84343e757a1f9
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-09-30 12:25:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd $HOME
-wget --no-check-certificate ""
+wget --no-check-certificate "https://github.com/mishakorzik/mishakorzik.menu.io/blob/master/%D0%A1%D0%B5%D1%80%D0%B2%D0%B5%D1%80/spower.zip?raw=true"
 mv 'spower.zip?raw=true' spower.zip
 unzip spower.zip
-echo 'move the iphack folden to the /site-packages/ folder'
+echo 'move the spower folden to the /site-packages/ folder'
